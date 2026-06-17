f922e0e1bf710812ee0420e0b1957e50ac0b1ff1
simone <simo@isw2>
2026-06-17 09:49:08 +0200

    added test suite

---
+++ openjpa-kernel/src/test/java/org/apache/openjpa/kernel/jpql/TestSuite.java
@@ -0,0 +1,11 @@
+package org.apache.openjpa.kernel.jpql;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        TestParsedJPQL.class
+})
+public class TestSuite {
+}
